863f653f1b4845afaf74627b7c48b38f936e99d0
fanciscogonzalezp <franciscogonzalezparra0@gmail.com>
2025-02-18 11:37:15 +0100

    Creado el component formNew, maquetado y con la funcionalidad completa

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormNewComponent } from "../form-new/form-new.component";
+import { INew } from '../../interfaces/inew.interface';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [FormNewComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
 
+  getNew(newPost: INew){
+    console.log("Recogiendo nuevo post: ", newPost)
+  }
 }
