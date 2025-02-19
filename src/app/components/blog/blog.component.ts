1f5600d2cc5460da30e1a9cb4e7ecfa87b1e37fc
fanciscogonzalezp <franciscogonzalezparra0@gmail.com>
2025-02-19 15:19:31 +0100

    Cambio datos iniciales prueba

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -12,8 +12,8 @@ import { ListNewComponent } from "../list-new/list-new.component";
 export class BlogComponent {
 
   listNews: INew[] = [
-    {title: 'Mi viaje a Islandia', image:'https://cdn.pixabay.com/photo/2019/11/24/14/03/nature-4649472_960_720.jpg', message:'Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu luctus laoreet turpis nullam massa sapien lobortis. Vel tempus integer hac montes litora. Maximus faucibus nostra auctor tincidunt netus duis. Ullamcorper maximus per diam nulla malesuada', date:'18/02/2025 10:50'},
-    {title: 'Mi viaje a China', image:'https://cdn.pixabay.com/photo/2019/03/14/18/15/landscape-4055549_960_720.jpg', message:'Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu luctus laoreet turpis nullam massa sapien lobortis. Vel tempus integer hac montes litora. Maximus faucibus nostra auctor tincidunt netus duis. Ullamcorper maximus per diam nulla malesuada. Lobortis bibendum massa dui dis vulputate dictum. Pulvinar arcu amet purus a viverra', date:'18/02/2025 12:30'}
+    {title: 'Mi viaje a Islandia', image:'https://cdn.pixabay.com/photo/2019/11/24/14/03/nature-4649472_960_720.jpg', message:'Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu luctus laoreet turpis nullam massa sapien lobortis. Vel tempus integer hac montes litora. Maximus faucibus nostra auctor tincidunt netus duis. Ullamcorper maximus per diam nulla malesuada', date:'2024/02/18'},
+    {title: 'Mi viaje a China', image:'https://cdn.pixabay.com/photo/2019/03/14/18/15/landscape-4055549_960_720.jpg', message:'Lorem ipsum odor amet, consectetuer adipiscing elit. Arcu luctus laoreet turpis nullam massa sapien lobortis. Vel tempus integer hac montes litora. Maximus faucibus nostra auctor tincidunt netus duis. Ullamcorper maximus per diam nulla malesuada. Lobortis bibendum massa dui dis vulputate dictum. Pulvinar arcu amet purus a viverra', date:'2023/07/10'}
   ]
 
   getNew(newPost: INew){
